917279ef1a1c70cae95f18c6e5769ca53ad70c30
Valentijn Manshande <valentijnmanshande@MacBook-Pro-van-Valentijn.local>
2020-01-23 11:35:01 +0100

    added doctrine paginator bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
