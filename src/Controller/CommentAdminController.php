8bcdeaad6efd3f709abf0cbfab86b8a31430d284
Valentijn Manshande <valentijnmanshande@MacBook-Pro-van-Valentijn.local>
2020-02-03 11:10:33 +0100

    keep stuff up to date with tutorial

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_ADMIN_COMMENT")
  */
 
 class CommentAdminController extends BaseController
