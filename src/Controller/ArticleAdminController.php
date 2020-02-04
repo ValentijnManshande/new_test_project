f28f6d1bfa4e126532dcf555e6c85c25cd2c4e0c
Valentijn Manshande <valentijnmanshande@MacBook-Pro-van-Valentijn.local>
2020-02-04 09:23:52 +0100

    finishing up security tutorial, starting with Form tutorial

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -4,26 +4,70 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use App\Form\ArticleFormType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @IsGranted("ROLE_ADMIN_ARTICLE")
- */
 
 class ArticleAdminController extends BaseController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
-    {
-        die('to do');
 
-        return new Response(sprintf('Hiya, new article id: %d slug: %s',
-        $article->getID(),
-        $article->getSlug()));
+    public function new(EntityManagerInterface $em, Request $request)
+    {
+       $form = $this->createForm(ArticleFormType::class);
+
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() && $form->isValid()){
+           $data = $form->getData();
+
+           $article = new Article();
+           $article->setTitle($data['title']);
+           $article->setContent($data['content']);
+           $article->setAuthor($this->getUser());
+
+           $em->persist($article);
+           $em->flush();
+
+           return $this->redirectToRoute('app_homepage');
+       }
+
+       return $this->render('article_admin/new.html.twig',[
+           'articleForm' => $form->createView(),
+       ]);
+
+    }
+
+    /**
+     * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
+     */
+    public function edit(Article $article)
+    {
+
+        $this->denyAccessUnlessGranted('MANAGE', $article);
+
+        dd($article);
+    }
+
+    /**
+     * @Route{"/admin/article")
+     */
+
+    public function list (ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles
+        ]);
     }
 }
